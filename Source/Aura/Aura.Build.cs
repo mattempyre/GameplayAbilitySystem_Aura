9081794f15f8f22b4062025d4dc8b782820ab14e
Matthew Fogarty <mattempyre@gmail.com>
2023-05-21 13:48:41 +0200

    Create Ability System Component C++ and Attribute Set C++ classes

--- Source/Aura/Aura.Build.cs
+++ Source/Aura/Aura.Build.cs
@@ -10,7 +10,7 @@ public class Aura : ModuleRules
 
 		PublicDependencyModuleNames.AddRange(new[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] { });
+		PrivateDependencyModuleNames.AddRange(new[] { "GameplayAbilities", "GameplayTags", "GameplayTasks" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
---
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -0,0 +1,5 @@
+// Copyright Empyre Games
+
+
+#include "AbilitySystem/AuraAttributeSet.h"
+
